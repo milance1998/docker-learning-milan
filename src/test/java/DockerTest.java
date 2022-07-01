406f1af690c3d1359e1f9edc7cdefa04f9303259
Milan Stojanovic <milan.98.stojanovic@gmail.com>
2022-07-01 10:26:25 +0200

    selenoid ui setup

--- src/test/java/DockerTest.java
+++ src/test/java/DockerTest.java
@@ -4,29 +4,48 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class DockerTest {
 
-    WebDriver driver;
+    public WebDriver driver;
 
     @BeforeEach
-    public void setUp() {
-        String userDir = System.getProperty("user.dir");
-        System.out.println(userDir);
-        System.setProperty("webdriver.chrome.driver", userDir + "/drivers/chromedriver-linux-64bit");
+    public void setUp() throws MalformedURLException {
+//        String userDir = System.getProperty("user.dir");
+//        System.out.println(userDir);
+//        System.setProperty("webdriver.chrome.driver", userDir + "/drivers/chromedriver-linux-64bit");
+        String selenoid = "http://localhost:4444/wd/hub";
 
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+        chromeOptions.setCapability(CapabilityType.BROWSER_VERSION, "101.0");
+        chromeOptions.setCapability("enableVNC", true);
+        chromeOptions.setCapability("enableVideo", false);
+        driver = new RemoteWebDriver(
+                URI.create(selenoid).toURL(),
+                chromeOptions);
         driver.manage().window().maximize();
+
+
+//        driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.manage().window().maximize();
     }
 
 
     @Test
     public void exampleTest() throws InterruptedException {
         driver.get("https://www.google.com");
-        Thread.sleep(5000);
+        Thread.sleep(10000);
     }
 
     @AfterEach
